d958c09dd0869538c4f0bcc88a7d92d7d6b22fa0
0himu1 <0himu1@gmail.com>
2024-05-18 11:34:26 +0600

    Code review done ready to merg with v0.001

--- src/routes/WhatsAppMessage.js
+++ src/routes/WhatsAppMessage.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const { sendTemplateToLead } = require('../controller/whatsAppMessageController');
 const {
     sendTextWaMessage,
     sendImageWaMessage,
     sendTemplateWaMessage,
 } = require('../controller/whatsAppController');
-const upload = require('../config/multerconfig');
+
+// Internal Imports
+// const { checkLogin } = require('../middlewares/auth/checkLogin');
 
 // Router Declearation
-const WaMessageRouter = express.Router();
+const wpMessageRouter = express.Router();
 
-// send a message to lead
-WaMessageRouter.post('/text', sendTextWaMessage);
-WaMessageRouter.post('/image', sendImageWaMessage);
-// WaMessageRouter.post('/template', sendTemplateWaMessage);
-WaMessageRouter.post('/template', upload.single('image'), sendTemplateWaMessage);
+// send text message to lead
+wpMessageRouter.post('/text', (req, res) => {
+    // send text message logic
+    console.log('Request Body: ', req.body);
+});
+wpMessageRouter.post('/text', sendTextWaMessage);
+wpMessageRouter.post('/image', sendImageWaMessage);
+wpMessageRouter.post('/template', sendTemplateWaMessage);
+// send template message to a custommer
+wpMessageRouter.post('/:id', sendTemplateToLead);
 
-module.exports = WaMessageRouter;
+module.exports = wpMessageRouter;
