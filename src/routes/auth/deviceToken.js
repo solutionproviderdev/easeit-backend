1ffb7e6a17296915d1ac933e0b44ed695036f728
0himu1 <0himu1@gmail.com>
2025-04-10 20:20:59 +0600

    Add mobile device token functionality to user schema and controller

--- src/routes/auth/deviceToken.js
+++ src/routes/auth/deviceToken.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {
     addDeviceToken,
     removeDeviceToken,
+    addMobileDeviceToken,
 } = require('../../controller/auth/deviceTokenController');
 const { checkAuth } = require('../../middlewares/auth/checkAuth');
 
@@ -12,5 +13,8 @@ deviceTokenRouter.post('/', checkAuth, addDeviceToken);
 
 // Route to remove a device token for a user
 deviceTokenRouter.post('/remove', checkAuth, removeDeviceToken);
+
+// add mobile device token
+deviceTokenRouter.post('/mobile', checkAuth, addMobileDeviceToken);
 
 module.exports = deviceTokenRouter;
