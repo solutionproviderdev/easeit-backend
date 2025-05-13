e84d8f91cf7e0dde9e44750235224143ef17ddd8
salmanfursi <salmanfursipro@gmail.com>
2025-05-13 17:44:51 +0600

    discount route copon folder crud apis page added !

--- src/routes/discountRoutes/discountRoutes.js
+++ src/routes/discountRoutes/discountRoutes.js
@@ -8,12 +8,19 @@ const {
     getActiveDiscounts,
     updateApprovalStatus,
     updateDiscountStatus,
+    getCouponById,
+    deleteCoupon,
+    updateCoupon,
 } = require('../../controller/discountController');
 
 // Public routes
 discountRouter.get('/active', getActiveDiscounts);
 discountRouter.post('/validate', validateCoupon);
 discountRouter.get('/applicable', findApplicableDiscounts);
+
+discountRouter.get('/coupon/:id', getCouponById);
+discountRouter.delete('/coupon/:id', deleteCoupon);
+discountRouter.put('/coupon/:id', updateCoupon);
 
 // Admin routes
 discountRouter.post('/create', createDiscount);
