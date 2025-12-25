116c8632f07beca19728186183f5bc76a24a76c7
salmanfursi <salmanfursipro@gmail.com>
2025-12-25 10:38:03 +0600

    solution provider online landing page all url are added

--- src/index.js
+++ src/index.js
@@ -90,6 +90,10 @@ app.use(
 				'http://192.168.68.130:5000',
 				'http://192.168.68.130',
 				'http://localhost:5173',
+				'http://www.solutionprovider.com.bd',
+				'http://solutionprovider.online',
+				'http://www.solutionprovider.online',
+				'http://solutionprovider.com.bd'
 			];
 			if (!origin || allowedOrigins.indexOf(origin) !== -1) {
 				callback(null, true);
